3ac8f89387ff999772eb491da32a934546e3d13c
cameron bradford <kylocam0712@gmail.com>
2025-04-30 07:41:05 -0500

    Initial Commit

---
+++ loginAppleGoogleExperiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginAppleGoogleExperiment
+//
+//  Created by CAMERON BRADFORD on 4/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ loginAppleGoogleExperimentTests/loginAppleGoogleExperimentTests.swift
@@ -0,0 +1,17 @@
+//
+//  loginAppleGoogleExperimentTests.swift
+//  loginAppleGoogleExperimentTests
+//
+//  Created by CAMERON BRADFORD on 4/30/25.
+//
+
+import Testing
+@testable import loginAppleGoogleExperiment
+
+struct loginAppleGoogleExperimentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
